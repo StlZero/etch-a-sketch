c1885ec5f3db0a1b2fa430696810e2836d5b9a2d
StlZero <112057152+StlZero@users.noreply.github.com>
2023-10-18 05:32:55 +0700

    Finished working on event listener

--- scripts.js
+++ scripts.js
@@ -2,25 +2,49 @@
 const grid_container = document.querySelector('.grid-container');
 
 
-// Generates square divs
-function generateDiv() {
+
+// Generates a grid of square divs
+for (i = 0; i < 100; i++) {
   let div1 = document.createElement('div');
   div1.classList.add('grid-div');
+  div1.setAttribute('draggable', 'false');
     
   grid_container.appendChild(div1);
 }
 
-
-// Generates a grid of square divs
-function generateGrid() {
-  
-  for (i = 0; i < 100; i++) {
-    generateDiv();
-  }
-}
-
-generateGrid();
+// grid_container.addEventListener('click', (e) => {
+//   e.target.style.background = 'blue';
+// });
 
 
-// Event on click -> changes color
+let div1 = document.querySelectorAll('.grid-div');
+
+
+// Change color of divs when hovering over divs while holding down mouse button
+div1.forEach(div => {
+  div.addEventListener('mouseover', (e) => {
+    if (e.buttons === 1) {
+      e.target.style.background = 'blue';
+    }
+  });
+
+  div.addEventListener('mousedown', (e) => {
+    e.target.style.background = 'blue';
+  })
+
+});
+
+
+
+// Disable dragging divs
+div1.forEach(div => {
+  div.addEventListener('dragstart', (e) => {
+    e.preventDefault();
+
+  });
+});
+
+
+
+
 
